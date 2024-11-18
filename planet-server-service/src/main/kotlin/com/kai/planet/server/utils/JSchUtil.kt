a9c5198ca4e87b1801e00a07603e14163a0dbabc
ForestAndForest <136168956+ForestAndForest@users.noreply.github.com>
2024-11-18 20:34:44 +0800

    重命名各服务目录名

--- planet-server-service/src/main/kotlin/com/kai/planet/server/utils/JSchUtil.kt
+++ planet-server-service/src/main/kotlin/com/kai/planet/server/utils/JSchUtil.kt
@@ -20,7 +20,7 @@ import java.nio.file.Paths
 
 object JSchUtil {
 
-    const val SESSION_TIMEOUT = 5000
+    const val SESSION_TIMEOUT = 30000
     const val CONNECT_TIMEOUT = 3000
 
     /**
@@ -90,80 +90,48 @@ object JSchUtil {
 
 
     fun scpTo(session: Session, source: String, destination: String) {
-        println("scpTo 1") // 1
         var fileInputStream: FileInputStream? = null
-        println("scpTo 2") // 2
         var channel: ChannelExec? = null
-        println("scpTo 3") // 3
         try {
             channel = openExecChannel(session)
-            println("scpTo 4") // 4
 
             val out = channel.outputStream
-            println("scpTo 5") // 5
             val `in` = channel.inputStream
-            println("scpTo 6") // 6
             var command = "scp -t $destination"
-            println("scpTo 7") // 7
             channel.setCommand(command)
-            println("scpTo 8") // 8
             channel.connect(CONNECT_TIMEOUT)
-            println("scpTo 9") // 9
 
             if (checkAck(`in`) != 0) return
-            println("scpTo 10") // 10
             val file = File(source)
-            println("scpTo 11") // 11
             val fileSize = file.length()
-            println("scpTo 12") // 12
             command = "C0644 $fileSize ${file.name}\n"
-            println("scpTo 13") // 13
             out.write(command.toByteArray())
-            println("scpTo 14") // 14
             out.flush()
-            println("scpTo 15") // 15
 
             if (checkAck(`in`) != 0) return
-            println("scpTo 16") // 16
 
             fileInputStream = FileInputStream(source)
-            println("scpTo 17") // 17
 
             val buf = ByteArray(1024)
-            println("scpTo 18") // 18
             var sum: Long = 0
-            println("scpTo 19") // 19
             while (true) {
                 val len = fileInputStream.read(buf)
-                println("scpTo 20") // 20
                 if (len <= 0) break
-                println("scpTo 21") // 21
                 out.write(buf, 0, len)
-                println("scpTo 22") // 22
                 sum += len
-                println("scpTo 23") // 23
             }
             buf[0] = 0
-            println("scpTo 24") // 24
             out.write(buf, 0, 1)
-            println("scpTo 25") // 25
             out.flush()
-            println("scpTo 26") // 26
 
             if (checkAck(`in`) != 0) return
-            println("scpTo 27") // 27
         } catch (e: Exception) {
-            println("scpTo 28") // 28
             throw CustomException(ServerCustomExceptionCode.SESSION_NULL)
         } finally {
-            println("scpTo 29") // 29
             closeInputStream(fileInputStream)
-            println("scpTo 30") // 30
             disconnect(channel)
-            println("scpTo 31") // 31
         }
     }
-
 
     /**
      * scp获取远程文件到指定目录
